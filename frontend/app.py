0a17fdd71958ddce3c29bf6c6b1ea8b5c0805797
Sergey Babenko <mc.vertix@gmail.com>
2011-09-24 15:48:07 +0400

    Add simple handler that renders data.

---
+++ frontend/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2.5
+# encoding: utf-8
+"""
+app.py
+
+Copyright (c) its authors.
+All rights reserved.
+"""
+
+import logging
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from view import by_region
+
+application = webapp.WSGIApplication(
+    [('/', by_region.ByRegionView),
+    ],
+    debug=True)
+
+
+def main():
+  logging.getLogger().setLevel(logging.DEBUG)
+  run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
