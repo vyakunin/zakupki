a2d469997be9f16a0c9680f20323f92638358141
Sergey Babenko <mc.vertix@gmail.com>
2011-09-24 11:39:24 +0400

    Add example product.xml.

---
+++ frontend/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.5
+# encoding: utf-8
+"""
+app.py
+
+Copyright (c) Sergey Babenko and Vladimir Yakunin 2011.
+All rights reserved.
+"""
+
+import sys
+import os
+
+
+print 'Content-Type: text/plain'
+print ''
+print 'Hello, world!'
