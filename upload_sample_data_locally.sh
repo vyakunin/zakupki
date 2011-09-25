dbd63fc4f6168862b0fc2fa3dc335334c8c588b2
Sergey Babenko <mc.vertix@gmail.com>
2011-09-25 13:39:43 +0400

    Change uploading script.

--- upload_sample_data_locally.sh
+++ upload_sample_data_locally.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-GAE=/home/filipovskii_off/Soft/google_appengine
+GAE=/usr/local/google_appengine
+# GAE=/home/filipovskii_off/Soft/google_appengine
 $GAE/appcfg.py upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/supplier.csv --kind=Supplier --config_file=frontend/bulkloader.yaml --noisy 
 $GAE/appcfg.py upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/customer.csv --kind=Customer --config_file=frontend/bulkloader.yaml --noisy 
 $GAE/appcfg.py upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/expense.csv --kind=Expense --config_file=frontend/bulkloader.yaml --noisy
