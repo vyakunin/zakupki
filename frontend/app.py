064df0ac80227feb1fcb779a165ddf828c301e7a
Sergey Babenko <mc.vertix@gmail.com>
2011-09-25 09:35:34 +0400

    Add HomePageView.

--- frontend/app.py
+++ frontend/app.py
@@ -12,13 +12,15 @@ import os
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from view import homepage
 from view import customers
 from view import regions
 from view import suppliers
 
 
 application = webapp.WSGIApplication(
-    [('/regions.json', regions.ByRegionView),
+    [('/', homepage.HomePageView),
+     ('/regions.json', regions.ByRegionView),
      ('/region_month.json', regions.ByMonthView),
      ('/top_customers', customers.TopCustomerView),
      ('/top_suppliers', suppliers.TopSuppliersView),
