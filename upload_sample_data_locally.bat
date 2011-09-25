0266988f8720be5eb8bf6131ae82c0b07b6bdd77
Valentin <v.koloman@gmail.com>
2011-09-25 12:57:34 +0400

    ????????? ? html ????  ??????? ? css

--- upload_sample_data_locally.bat
+++ upload_sample_data_locally.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/supplier.csv --kind=Supplier --config_file=frontend/bulkloader.yaml
-"C:\Program Files (x86)\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/customer.csv --kind=Customer --config_file=frontend/bulkloader.yaml
-"C:\Program Files (x86)\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/expense.csv --kind=Expense --config_file=frontend/bulkloader.yaml
+"C:\Program Files (x86)\Google\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/supplier.csv --kind=Supplier --config_file=frontend/bulkloader.yaml
+"C:\Program Files (x86)\Google\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/customer.csv --kind=Customer --config_file=frontend/bulkloader.yaml
+"C:\Program Files (x86)\Google\google_appengine\appcfg.py" upload_data --url=http://localhost:8080/_ah/remote_api --filename=sample_data/expense.csv --kind=Expense --config_file=frontend/bulkloader.yaml
