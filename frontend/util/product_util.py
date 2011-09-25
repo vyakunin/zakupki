0c15ff84e96b661b5133e24013c6f69736cdeed0
Sergey Babenko <mc.vertix@gmail.com>
2011-09-25 16:47:02 +0400

    Fix

--- frontend/util/product_util.py
+++ frontend/util/product_util.py
@@ -17,7 +17,7 @@ class ProductCategory(object):
 
   @staticmethod
   def ByCode(code):
-    return ProductCategory.CATEGORY_BY_CODE[code[:2]]
+    return ProductCategory.CATEGORY_BY_CODE.get(code[:2], 'Другое')
 
 ProductCategory.CATEGORIES = [
   ProductCategory('01', 'Сельское хозяйство,  охота и связанная с этим деятельность по предоставлению услуг'),
