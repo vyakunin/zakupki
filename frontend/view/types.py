43fa26be5a8b50465434d03b9654b26c685e0f0c
Sergey Babenko <mc.vertix@gmail.com>
2011-12-18 14:48:22 +0400

    Step 5 - extract types page.

--- frontend/view/types.py
+++ frontend/view/types.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-homepage.py
+types.py
 
 Copyright (c) 2011 All its authors.
 All rights reserved.
@@ -9,6 +9,8 @@ All rights reserved.
 
 import os
 
+from datetime import datetime
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
@@ -16,9 +18,9 @@ import model
 from view import config
 
 
-class HomePageView(webapp.RequestHandler):
-  """Renders homepage."""
+class TypesView(webapp.RequestHandler):
+  """Renders types page."""
   def get(self):
     self.response.headers['Content-Type'] = 'text/html;charset=utf-8'
-    path = os.path.join(os.path.dirname(__file__), '../templates/homepage.html')
+    path = os.path.join(os.path.dirname(__file__), '../templates/types.html')
     self.response.out.write(template.render(path, {'config': config.Config}))
